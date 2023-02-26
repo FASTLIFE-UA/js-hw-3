ba6e8c8944b59223b67836c4e2be99f2b15a3681
FASTLIFE <124807131+FASTLIFE-UA@users.noreply.github.com>
2023-02-26 16:03:54 +0200

    Add files via upload

---
+++ ex1/script.js
@@ -0,0 +1,6 @@
+const name = "Генератор";
+const price = 1000;
+const newPrice = 2000;
+
+console.log(`Обрано ${name}, ціна за одиницю ${price} кредитів`);
+console.log(`Обрано ${name}, ціна за одиницю ${newPrice} кредитів`);
